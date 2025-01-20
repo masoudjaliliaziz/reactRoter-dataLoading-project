94eadbde01e2708b5870262481ef87138163db69
masoudjaliliaziz <masoudjalili0781@gmail.com>
2025-01-20 16:11:54 +0330

    adding the feature for deleting the pizaa from cart

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -34,7 +34,8 @@ const cartSlice = createSlice({
 
 export const getTotalQuantity = (state) =>
   state.cart.cart.reduce((sum, item) => item.quantity + sum, 0);
-
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
 export const getTotalPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
 export const getCart = (state) => state.cart.cart;
